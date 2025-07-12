b91029b3930b6c6f832291b3705a3728b649b483
Mohamed-practice <mohammedimrancr7@gmail.com>
2025-07-12 19:28:26 +0530

    Updated

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin: ['http://localhost:3000', 'http://localhost:5173', 'http://127.0.0.1:5173','https://scrapconnect.pages.dev'],
   credentials: true
 }));
 app.use(express.json());
